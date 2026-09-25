7401f3a1411522fd4a5f5adcc06950ee58024f3d
Nicodemo-Campos <75339298+Nicodemo-Campos@users.noreply.github.com>
2026-09-25 14:02:03 -0300

    vulkan: expose latest interpolated image

--- gpu/vulkan/layer/src/passthrough_pipeline.hpp
+++ gpu/vulkan/layer/src/passthrough_pipeline.hpp
@@ -80,6 +80,7 @@ public:
         MotionValidationSample& sample) noexcept;
     [[nodiscard]] bool bidirectional_warp_enabled() const noexcept;
     [[nodiscard]] bool interpolated_frame_ready() const noexcept;
+    [[nodiscard]] VkImage latest_interpolated_image() const noexcept;
     [[nodiscard]] bool read_warp_validation(
         std::uint32_t slot_index,
         WarpValidationSample& sample) noexcept;
