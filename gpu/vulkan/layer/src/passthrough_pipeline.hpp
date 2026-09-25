2a52c08c24a3b1cee3e31078f0a30f90c3d43168
Nicodemo-Campos <75339298+Nicodemo-Campos@users.noreply.github.com>
2026-09-24 22:22:24 -0300

    vulkan: allow distinct compute output extent

--- gpu/vulkan/layer/src/passthrough_pipeline.hpp
+++ gpu/vulkan/layer/src/passthrough_pipeline.hpp
@@ -28,11 +28,13 @@ public:
         VkDevice device,
         PFN_vkGetDeviceProcAddr get_device_proc_addr,
         const VkPhysicalDeviceMemoryProperties& memory_properties,
-        VkExtent2D extent,
+        VkExtent2D source_extent,
+        VkExtent2D output_extent,
         VkFormat source_format,
         const std::vector<VkImage>& source_images) noexcept;
 
     [[nodiscard]] bool ready() const noexcept;
+    [[nodiscard]] VkExtent2D output_extent() const noexcept;
     [[nodiscard]] bool record(
         VkCommandBuffer command_buffer,
         std::uint32_t slot_index) const noexcept;
@@ -57,7 +59,8 @@ private:
 
     VkDevice device_ = VK_NULL_HANDLE;
     VkPhysicalDeviceMemoryProperties memory_properties_{};
-    VkExtent2D extent_{};
+    VkExtent2D source_extent_{};
+    VkExtent2D output_extent_{};
     VkFormat source_format_ = VK_FORMAT_UNDEFINED;
 
     PFN_vkCreateImage create_image_ = nullptr;
